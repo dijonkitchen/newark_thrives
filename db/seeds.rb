de172ebb0015872743c9e49b2ddde887fe6152fd
Bruno Zatta <bzatta@gmail.com>
2016-03-26 14:49:57 -0400

    add faker to gemfile, add program id to program details, add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+20.times do
+  Program.create(name: Faker::Company.name, link: Faker::Internet.url, start_date: Faker::Date.forward(rand(1..100)), end_date: Faker::Date.forward(rand(100..200)))
+end
+
+programs = Program.all
+
+programs.each do |program|
+  program.program_detail = ProgramDetail.create(address: Faker::Address.street_address + Faker::Address.city, phone_number: Faker::PhoneNumber.cell_phone, description: Faker::Hipster.sentence, days_offered: Faker::Date.between(2.days.ago, Date.today), time: "8:00AM - 6:00PM", location_type: Faker::Hipster.word, meals_offered: Faker::Hipster.word, grades_served: Faker::Hipster.word, enrollment_type: Faker::Hipster.word, enrollment_period: Faker::Hipster.word, enrollment_requirements: Faker::Hipster.word, cost: Faker::Hipster.word, focus: Faker::Hipster.sentence(3))
+end
