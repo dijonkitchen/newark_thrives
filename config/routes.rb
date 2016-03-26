7af4caec5a416551950840592cd75a4336a8b256
dijonkitchen <jschen2008@gmail.com>
2016-03-26 13:38:15 -0400

    Add basic routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
 
-  
+  root 'programs#index'
+
+  resources :programs
+    
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
